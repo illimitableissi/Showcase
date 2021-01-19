a476bf2186681518689259446dfa8868db958007
SOVEREIGN <isimemeni@gmail.com>
2021-01-18 20:21:10 -0500

    added javascript file for reference

---
+++ applications.js
@@ -0,0 +1,163 @@
+const websites = [
+    {   
+    name: "Natours",
+    gitHub: "https://github.com/illimitableissi/Natours_HTML",
+    link: "https://illimitableissi.github.io/Natours_HTML/",
+    img: "images/natours.PNG"
+    },
+    {   
+    name: "Trillo",
+    gitHub: "https://github.com/illimitableissi/Trillo-HTML",
+    link: "https://illimitableissi.github.io/Trillo-HTML/",
+    img: "images/trillo.PNG"
+    },
+    {   
+    name: "Nexter",
+    gitHub: "https://github.com/illimitableissi/Nexter-HTML",
+    link: "https://illimitableissi.github.io/Nexter_HTML/",
+    img: ""
+    },
+]
+
+
+const pythonApps = [
+    {   
+    name: "Blackjack",
+    gitHub: "https://github.com/illimitableissi/Python_Blackjack",
+    img: "images/background1.jpg"
+    },
+    {   
+    name: "Tic Tac Toe",
+    gitHub: "https://github.com/illimitableissi/Python-TicTacToe",
+    img: "images/background1.jpg"
+    },
+    {   
+    name: "I Declare War",
+    gitHub: "https://github.com/illimitableissi/Python_War",
+    img: "images/background1.jpg"
+    },
+]
+
+
+const reactApps = [
+    {
+        name: "Weekendr",
+        appLink: "https://illimitableissi.github.io/Weekndr/#/",
+        gitHub: "https://github.com/illimitableissi/Weekndr",
+        imgSrc: "images/weekndr.PNG",
+        message: "The Weeknd fan page powered by React and Spotify API",
+        tech: "MongoDB, React.js, Express.js, Node, CSS, React-Bootstrap, "
+    },
+    {
+        name: "Profyle",
+        appLink: "https://profyle.herokuapp.com/",
+        gitHub: "https://github.com/illimitableissi/Profyle",
+        imgSrc: "images/homescreen.PNG",
+        message: "Profyle is an About Me page creation app",
+        tech: "MongoDB, React.js, Express.js, Node, CSS, React-Bootstrap, "
+    },
+    {
+        name: "Kangaroo",
+        appLink: "https://kangarooleasing.herokuapp.com/",
+        gitHub: "https://github.com/illimitableissi/Kangaroo",
+        imgSrc: "images/kangaroo.PNG",
+        message: "Kangaroo helps lease holders looking to sub-lease their property find lease seekers",
+        tech: "MongoDB, React.js, Express.js, Node, CSS, Bootstrap, "
+    }
+]
+
+
+const information = [
+    {
+        name: "Clicky Game",
+        appLink: "https://illimitableissi.github.io/React-Clicky-Game/",
+        gitHub: "https://github.com/illimitableissi/React-Clicky-Game.git",
+        imgSrc: "images/click.PNG",
+        message: "Click Game made through React.js",
+        tech: "Javascript, React.js, HTML, CSS, Bootstrap"
+    },
+    {
+        name: "Crystal Collector",
+        appLink: "https://illimitableissi.github.io/Save-the-Universe/",
+        gitHub: "https://github.com/illimitableissi/Save-the-Universe",
+        imgSrc: "images/universe.PNG",
+        message: "The app randomly picks a number and the user will have to click on the stones and match the number.",
+        tech: "HTML5, CSS, Javascript, jQuery, Bootstrap"
+    },
+    {
+        name: "Dragon Ball Trivia!",
+        appLink: "https://illimitableissi.github.io/TriviaGame/",
+        gitHub: "https://github.com/illimitableissi/TriviaGame.git",
+        imgSrc: "images/dbz.PNG",
+        message: "Dragon Ball Series trivia game.",
+        tech: "HTML5, CSS, Javascript, jQuery, Bootstrap"
+    },
+    {
+        name: "One Piece Pirates!",
+        appLink: "https://illimitableissi.github.io/GifTastic/",
+        gitHub: "https://github.com/illimitableissi/GifTastic.git",
+        imgSrc: "images/onepiece.PNG",
+        message: "User can click on a character name and gifs will populate from the GIPHY API.",
+        tech: "HTML5, CSS, Javascript, jQuery, Bootstrap, GIPHY API"
+    },
+    {
+        name: "Train Scheduler",
+        appLink: "https://illimitableissi.github.io/TrainScheduler/",
+        gitHub: "https://github.com/illimitableissi/TrainScheduler.git",
+        imgSrc: "images/train.PNG",
+        message: "Incorporates Firebase to host arrival and departure data. This website will provide up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.",
+        tech: "HTML5, CSS, Javascript, jQuery, Bootstrap, Firebase"
+    },
+    {
+        name: "TraveLite",
+        appLink: "https://usafowose.github.io/Project-1/",
+        gitHub: "https://github.com/illimitableissi/Travelite",
+        imgSrc: "images/travel.PNG",
+        message: "Users can enter flight information and will be shown various flights, weather forecasts, and images of their chosen location.",
+        tech: "HTML5, CSS, Javascript, jQuery, Materialize, Accuweather API, TravelPayouts API, Pixabay API"
+    },
+    {
+        name: "LIRI Bot-Node.js",
+        appLink: "#",
+        gitHub: "https://github.com/illimitableissi/liri-node-app.git",
+        imgSrc: "images/background1.jpg",
+        message: "Node app that allows users to see movie information, concerts, and music information from various APIs.",
+        tech: "Javascript, Node.js, Axios"
+    },
+    {
+        name: "MySQL+Node.js Bamazon",
+        appLink: "#",
+        gitHub: "https://github.com/illimitableissi/bamazon.git",
+        imgSrc: "images/background1.jpg",
+        message: "Back-end storefront built with MySQL.",
+        tech: "Javascript, Node.js, MySQL"
+    },
+    {
+        name: "Friend Finder",
+        appLink: "https://friend-finderi.herokuapp.com/",
+        gitHub: "https://github.com/illimitableissi/FriendFinder.git",
+        imgSrc: "images/background1.jpg",
+        message: "This full-stack site will take in results from your users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match.",
+        tech: "Javascript, Node.js, MySQL, jQuery, Express.js, HTML5, Bootstrap, CSS"
+    },
+    {
+        name: "Eat Da Burger!",
+        appLink: "https://burgerii.herokuapp.com/",
+        gitHub: "https://github.com/illimitableissi/burger",
+        imgSrc: "images/burger.PNG",
+        message: "Restaurant app that lets users input the names of burgers they'd like to eat.",
+        tech: "Javascript, Node.js, MySQL, jQuery, Express.js, HTML5, Bootstrap, CSS"
+    },
+    {
+        name: "The Krusty Krab",
+        appLink: "https://thekrustykrab.herokuapp.com/",
+        gitHub: "https://github.com/illimitableissi/The-Krusty-Krab",
+        imgSrc: "images/krusty.PNG",
+        message: "Spongebob Squarepants themed restaurant app.",
+        tech: "Javascript, Node.js, MySQL, jQuery, Express.js, HTML5, Bootstrap, Materialize, CSS"
+    }
+]
+
+    
+
+    
